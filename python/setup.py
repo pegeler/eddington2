aa3481b068d2957474d1a9c12085cfb2f592069f
pegeler <32426645+pegeler@users.noreply.github.com>
2020-04-11 01:07:28 -0400

    default setup.py to use setuptools

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(name="eddington",
       version="0.0",
       author="Paul Egeler",
       url="https://github.com/pegeler/eddington2/",
       license="GPL-3",
       package_dir = {"eddington": "src"},
-      packages = ["eddington"])
+      packages = ["eddington"],
+      keywords = [
+          "cycling",
+          "h-index",
+          "Durfee square",
+          "algorithms",
+          ]
+      )
