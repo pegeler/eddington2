0a9278c15808b6925911ead012b0e34dab8c5975
pegeler <32426645+pegeler@users.noreply.github.com>
2023-08-21 00:15:13 -0400

    adding a few functions to the python module

--- python/setup.py
+++ python/setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 
 setup(name='eddington',
-      version='2.0',
+      version='3.0',
       python_requires='>=3.9',
       author='Paul Egeler',
       description='Compute the Eddington number for cycling',
       url='https://github.com/pegeler/eddington2/',
       license='GPL-3',
       py_modules=['eddington'],
-      keywords = [
+      keywords=[
           'cycling',
           'h-index',
           'Durfee square',
