4dd970c936d2fcd2edabd589ec222acafe2d694c
pegeler <32426645+pegeler@users.noreply.github.com>
2023-11-08 01:24:36 -0500

    using tabs for input methods in web version

--- docs/eddington.js
+++ docs/eddington.js
@@ -1,13 +1,11 @@
-// Define Eddington function
-function eddington(rides)
-{
-  n = rides.length
-  E = 0
-  above = 0
-  H = new Array(n).fill(0)
+var eddington = function(rides) {
+  var n = rides.length
+  var E = 0
+  var above = 0
+  var H = new Array(n).fill(0)
 
   for ( i = 0; i < n; i++ ) {
-    ride = rides[i]
+    var ride = rides[i]
     if (ride > E) {
       above++
       if (ride < n) H[ride]++
@@ -22,39 +20,41 @@ function eddington(rides)
   return E
 }
 
-// Parse user input into integer array
-function textParse(text)
-{
+var parseText = function(text) {
   return text.split(/\r*\n|,/).map(ride => parseInt(ride))
 }
 
-// Process file and compute E
-function getFile()
-{
-  
-  file = document.getElementById('file-input').files[0]
-  reader = new FileReader()
-  
-  
-  reader.onload = function() 
-  {
-    rides = textParse(this.result)
-    document.getElementById('E').innerHTML = eddington(rides)    
+var getFile = function() {
+  var file = document.getElementById('file-input').files[0]
+  var reader = new FileReader()
+
+  reader.onload = function() {
+    document.getElementById('E').innerHTML = eddington(parseText(this.result))
   }
 
   reader.readAsText(file)
-
 }
 
-// Create event listener for file-input element
-window.onload = function() 
-{
-  document.getElementById('file-input').addEventListener('change', getFile)
-}
-
-// Process textarea element and compute E
-function submitText() 
-{
-   rides = textParse(document.getElementById('text-input').value)
+var submitText = function() {
+   var rides = parseText(document.getElementById('text-input').value)
    document.getElementById('E').innerHTML = eddington(rides)
+}
+
+var loadTab = function(evt, tab_content_id) {
+    var tab_contents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tab_contents.length; i++) {
+      tab_contents[i].style.display = "none";
+    }
+    document.getElementById(tab_content_id).style.display = "block";
+
+    var tabs = document.getElementsByClassName("tab");
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].className = tabs[i].className.replace(" active", "");
+    }
+    evt.currentTarget.className += " active";
+}
+
+window.onload = function() {
+  document.getElementById('default-tab').click()
+  document.getElementById('file-input').addEventListener('change', getFile)
 }
